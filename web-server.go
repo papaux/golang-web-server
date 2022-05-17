e548383fe2f4b602ae64b26204ae255f880be022
Geoffrey Papaux <geoffrey.papaux@gmail.com>
2022-05-17 22:36:49 +0200

    Update web-server.go

--- web-server.go
+++ web-server.go
@@ -3,13 +3,15 @@ package main
 import (
 	"io"
 	"net/http"
+	"fmt"
 )
 
 func main() {
+	fmt.Printf("Starting web server...")
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	io.WriteString(w, "Hello world! ")
 }
